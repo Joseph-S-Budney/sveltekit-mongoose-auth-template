331f6f466e0bd8bdb22b523f5365e550305faa85
unknown <jo.s.budney@baypath.net>
2024-04-21 20:41:59 -0400

    added a layout file

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import "../app.pcss";
+    import { enhance } from "$app/forms";
+</script>
+
+<form method="POST" use:enhance>
+    <button>Sign Out</button>
+</form>
+
+<slot></slot>
