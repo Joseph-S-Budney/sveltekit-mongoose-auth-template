02557d91143a419295ab9424ed408ea224465d12
Budney <Jo.S.Budney@baypath.net>
2024-04-23 09:46:19 -0400

    hides header if not signed in

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
     import "../app.pcss";
     import Header from "./Header.svelte";
+    export let data;
 </script>
 
 <div class="container-fluid">
-    <Header />
+    {#if data.user}
+        <Header />
+    {/if}
     <slot></slot>
 </div>
